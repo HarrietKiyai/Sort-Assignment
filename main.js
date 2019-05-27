0f9c5282a15d2258ee99b5d4347472fb54175469
Harriet C Kiyai <kharie65@gmail.com>
2019-05-27 15:59:17 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/*var myList = ["Harriet ", "Linda", "Abel", "George"];
+var unsorted = document.getElementById("unsorted");
+for (var i = 0; i < myList.length; i++) {
+    unsorted.innerHTML += "<li>" + myList[i] + "</li>";
+}
+
+function sortList() {
+    myList.sort();
+    var sorted = document.getElementById("sorted");
+    sorted.innerHTML = "";
+    for (var i = 0; i < myList.length; i++) {
+        sorted.innerHTML += "<li>" + myList[i] + "</li>";
+    }
+}*/
